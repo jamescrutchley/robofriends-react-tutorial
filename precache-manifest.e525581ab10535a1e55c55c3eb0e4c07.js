91152e02e6056f3565ffb13268fba4b8091b24ff
James Crutchley <jamescrutchley98@gmail.com>
2022-06-30 11:33:14 +1000

    Updates

---
+++ precache-manifest.e525581ab10535a1e55c55c3eb0e4c07.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "69865760339f01fd69ae",
+    "url": "/robotfriends-react-tutorial/static/css/main.ba3f61fb.chunk.css"
+  },
+  {
+    "revision": "69865760339f01fd69ae",
+    "url": "/robotfriends-react-tutorial/static/js/main.0b43d0b7.chunk.js"
+  },
+  {
+    "revision": "b55c97c3e415cce05542",
+    "url": "/robotfriends-react-tutorial/static/js/runtime~main.d5b8e43d.js"
+  },
+  {
+    "revision": "68f4a123e7bd1fe5eb6a",
+    "url": "/robotfriends-react-tutorial/static/css/2.14f37c3a.chunk.css"
+  },
+  {
+    "revision": "68f4a123e7bd1fe5eb6a",
+    "url": "/robotfriends-react-tutorial/static/js/2.d8acf6e2.chunk.js"
+  },
+  {
+    "revision": "db569b567d12d48e4e61",
+    "url": "/robotfriends-react-tutorial/static/js/3.d51c813c.chunk.js"
+  },
+  {
+    "revision": "ba85c44c8386c4afe97a6a88b3a37442",
+    "url": "/robotfriends-react-tutorial/static/media/BAUHS93.ba85c44c.TTF"
+  },
+  {
+    "revision": "5c36d2d71b56e854d2eb209587ece77c",
+    "url": "/robotfriends-react-tutorial/index.html"
+  }
+];
